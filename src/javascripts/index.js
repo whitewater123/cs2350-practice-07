5311c5c7c0f1f9232d51f5c79ea2183d2e4a13ce
Bluelight413 <was16006@byui.edu>
2020-03-27 12:30:55 -0600

    Final Submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,3 +5,51 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+
+const displayRandomBox = function() {
+    let r = Math.random()
+    if(r < .5) {
+        document.write('<div class="green box"></div>')
+    }
+    else {
+        document.write('<div class="blue box"></div>')
+    }
+}
+
+
+function displayNBoxesUsingFor(n) {
+    for(let i=0; i < n; i++) {
+        displayRandomBox()
+    }
+}
+
+function displayNBoxesUsingWhile(n) {
+    let j = 0;
+    while(j < n) {
+        displayRandomBox()
+        j++
+    }
+}
+
+function displayNBoxesUsingDoWhile(n) {
+    let k = 0
+    do {
+        displayRandomBox()
+        k++
+    } while(k < n)
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+displayNBoxesUsingFor(15)
+displayNBoxesUsingWhile(12)
+displayNBoxesUsingDoWhile(15)
+
+
+const square = function(x) {
+    return x * x
+}
+
+alert(square(100))
